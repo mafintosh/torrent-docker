dab5890036adac3de2a98484a7330a62e6fcc25a
Mathias Buus <mathiasbuus@gmail.com>
2014-11-19 02:46:48 +0100

    single entrypoint

---
+++ bin.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var cmd = process.argv[2]
+
+process.argv.splice(2, 1)
+
+if (cmd === 'seed') require('./bin/seed')
+else if (cmd === 'create') require('./bin/create')
+else if (cmd === 'run') require('./bin/run')
+else console.error('Usage: docker-instant [cmd] [opts]')
