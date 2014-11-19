be985c4b25c3837e7300169cad3d5895053d5825
Mathias Buus <mathiasbuus@gmail.com>
2014-11-19 13:51:57 +0100

    boot alias

--- bin.js
+++ bin.js
@@ -6,5 +6,5 @@ process.argv.splice(2, 1)
 
 if (cmd === 'seed') require('./bin/seed')
 else if (cmd === 'create') require('./bin/create')
-else if (cmd === 'run') require('./bin/run')
+else if (cmd === 'run' || cmd === 'boot') require('./bin/run')
 else console.error('Usage: docker-instant [cmd] [opts]')
